7ed1e39ad7007017359385359f7c754048aa55bd
Federico <fededeniard@gmail.com>
2025-06-04 23:05:31 -0300

    Changed a text

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,8 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {/* Edit <code>src/App.tsx</code> and save to test HMR */}
+          This is a test on how to deploy a React app using AWS Amplify
         </p>
       </div>
       <p className="read-the-docs">
