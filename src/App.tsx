716c7e43741adf49a35a7d3f7528cb830ecd0744
Federico <fededeniard@gmail.com>
2025-06-05 01:16:02 -0300

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,51 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+import type { AuthUser } from "aws-amplify/auth";
+
+type AppProps = {
+  signOut?: () => void;
+  user?: AuthUser;
+};
+
+function App({ signOut, user }: AppProps) {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Prueba Amplify</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          {/* Edit <code>src/App.tsx</code> and save to test HMR */}
-          This is a test on how to deploy a React app using AWS Amplify
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <View className="App">
+      <Card>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <Heading level={1}>¡Bienvenido, {user?.username}!</Heading>
+        <h1>Prueba Amplify</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Esto es una prueba de cómo deployar una app React con AWS Amplify y
+            Cognito
+          </p>
+        </div>
+        <Button onClick={signOut}>Cerrar sesión</Button>
+      </Card>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
